daba6ffba58a5a56c06ed8e531fbc5e3f96524b4
Ayat Ospanov <ayat.ospanov@gmail.com>
2022-06-05 17:40:57 +0200

    Added firebase functions to the project
    Added a function to get flights info

---
+++ functions/src/index.ts
@@ -0,0 +1,31 @@
+import * as functions from "firebase-functions";
+import axios from "axios";
+
+export const getFlights = functions.https.onRequest((request, response) => {
+  let datetime = new Date(request.query["date"] as string);
+  if (isNaN(datetime.getFullYear())) {
+    datetime = new Date();
+  }
+  let date = datetime.toISOString().slice(0, 10);
+  axios
+    .get(
+      `https://www.swedavia.com/services/publicflightsboard/v2/departures/en/ARN/${date}`
+    )
+    .then((response) => {
+      let allFlights = response.data["flights"] || [];
+      return allFlights
+        .filter((flight: any) => flight["epiTerminal"]["Id"] === "5")
+        .map((flight: any) => {
+          return {
+            destination: flight["arrivalAirportEnglish"],
+            flightId: flight["flightId"],
+            scheduledDepartureTime: flight["departureTime"]["scheduledUtc"],
+          };
+        });
+    })
+    .then((flights_by_hour) => {
+      response.set("Cache-Control", "public, max-age=300, s-maxage=600");
+      response.json({ flights: flights_by_hour });
+    })
+    .catch(() => response.json({ flights: [] }));
+});
